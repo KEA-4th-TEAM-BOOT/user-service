cde37cc8569b1c93d9409ef2fac489300fde085c
seungwon7934 <sw7934@gmail.com>
2024-06-05 20:59:12 +0900

    BOOT-91 [Fix] 토큰값 해독 안되던 에러 해결

--- src/main/java/userservice/utils/TokenUtils.java
+++ src/main/java/userservice/utils/TokenUtils.java
@@ -13,7 +13,13 @@ public class TokenUtils {
     }
 
     public Long getUserIdFromToken(String token) {
-        return Long.valueOf(jwtTokenProvider.getUserId(token));
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);  // 'Bearer '를 제거합니다.
+        }
+        if (token != null) {
+            return Long.valueOf(jwtTokenProvider.getUserId(token));
+        }
+        return null;  // token이 null인 경우 null을 반환합니다.
     }
 
 }
